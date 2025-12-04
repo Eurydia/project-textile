57a2f94057b903f737d2d1e697bb89d0c7689bce
Thanakorn Phuttharaksa <crossdeltavi@gmail.com>
2025-12-04 14:41:05 +0100

    Remove Q and app/test directories and related files
    Deleted all files in the Q, app, and test directories, including configuration, source, public assets, and documentation. Also removed the .gitmodules file, indicating the removal of a submodule. This commit cleans up the repository by removing these components.

--- app/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "@mui/material";
-
-export default function Home() {
-  return (
-    <Container maxWidth="md">
-      <iframe width="100%" height="600" src="/posts/index.html"></iframe>
-    </Container>
-  );
-}

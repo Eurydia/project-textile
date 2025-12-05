1e668d48eab3b9fb467bddc08b7ff4b4d3e4ced2
Thanakorn Phuttharaksa <crossdeltavi@gmail.com>
2025-12-05 14:36:08 +0100

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-const q: Record<
-  string,
-  Array<{ label: string; path: Array<string> }>
-> = await fetch('/sitemap.json').then((r) => r.json())
 export const Route = createFileRoute('/')({
   component: App,
 })

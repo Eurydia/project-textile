91124307651eaa9441bbb5aa9dedc3c1484cf2f8
Thanakorn Phuttharaksa <crossdeltavi@gmail.com>
2025-12-05 08:32:36 +0100

    Update prebuild.ts

--- scripts/prebuild.ts
+++ scripts/prebuild.ts
@@ -15,45 +15,36 @@ import { JSDOM } from 'jsdom'
 const contentDir = posix.join(posix.dirname(process.cwd()), 'content')
 const tempDir = posix.join(contentDir, 'temp')
 console.debug(tempDir)
-try {
-  mkdirSync(tempDir)
-  for (const blog of globbySync(posix.join(contentDir, '**', '*.tex'), {
-    ignore: [posix.join(contentDir, 'temp', '**')],
-  })) {
-    const sg = blog.split(posix.sep).slice(1)
-    const bname = sg.at(-1)!
-    const stem = basename(bname, '.tex')
-    sg.pop()
-    const workingDir = posix.join(tempDir, ...sg, stem)
-    console.debug(sg, workingDir)
-    mkdirSync(workingDir, { recursive: true })
-    copyFileSync(blog, posix.join(workingDir, bname))
 
-    spawnSync('pdflatex', [bname], { cwd: workingDir })
-    spawnSync('lwarpmk', ['html'], { cwd: workingDir })
-    spawnSync('lwarpmk', ['clean'], { cwd: workingDir })
+for (const blog of globbySync(posix.join(contentDir, '**', '*.tex'), {
+  ignore: [posix.join(contentDir, 'temp', '**')],
+})) {
+  const sg = blog.split(posix.sep).slice(1)
+  const bname = sg.at(-1)!
+  const stem = basename(bname, '.tex')
+  sg.pop()
+  const workingDir = posix.join(tempDir, ...sg, stem)
+  console.debug(sg, workingDir)
+  mkdirSync(workingDir, { recursive: true })
+  copyFileSync(blog, posix.join(workingDir, bname))
 
-    const publicDir = posix.join(
-      posix.dirname(process.cwd()),
-      'public',
-      'content',
-      ...sg,
-    )
-    if (!existsSync(publicDir)) {
-      mkdirSync(publicDir, { recursive: true })
-    }
-    copyFileSync(
-      posix.join(workingDir, `${stem}.html`),
-      posix.join(publicDir, `${stem}.html`),
-    )
+  spawnSync('pdflatex', [bname], { cwd: workingDir })
+  spawnSync('lwarpmk', ['html'], { cwd: workingDir })
+  spawnSync('lwarpmk', ['clean'], { cwd: workingDir })
+
+  const publicDir = posix.join(
+    posix.dirname(process.cwd()),
+    'public',
+    'content',
+    ...sg,
+  )
+  if (!existsSync(publicDir)) {
+    mkdirSync(publicDir, { recursive: true })
   }
-} catch (e) {
-  console.debug(e)
-} finally {
-  rmSync(tempDir, {
-    recursive: true,
-    force: true,
-  })
+  copyFileSync(
+    posix.join(workingDir, `${stem}.html`),
+    posix.join(publicDir, `${stem}.html`),
+  )
 }
 
 const sitemap: Record<

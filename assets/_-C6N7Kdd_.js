a9607537f9a6a958fecca647fac34d36deace220
Eurydia <Eurydia@users.noreply.github.com>
2025-12-06 16:16:34 +0000

    Deploying to gh-pages from @ Eurydia/project-textile@9ebfdf339c0e3ecaa219dd597a3b0a920b17bdaa 🚀

--- assets/_-C6N7Kdd_.js
+++
@@ -1,1 +0,0 @@
-import{x as t,j as e}from"./index-BtfwwZ1V.js";import{u as n}from"./useTypesetOnLoad-D93KJ_9v.js";function a(){const{body:o}=t.useLoaderData();return n(),e.jsx("div",{dangerouslySetInnerHTML:{__html:o}})}export{a as component};
--- assets/useTypesetOnLoad-D93KJ_9v.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-BtfwwZ1V.js";const e=()=>{t.useEffect(()=>{(async()=>await window.MathJax.typesetPromise().catch(()=>{}))()},[])};export{e as u};

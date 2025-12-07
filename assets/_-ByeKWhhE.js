8c4326bfc4a0f5c2ffb252278a2f2b1ad313ca7c
Thanakorn Phuttharaksa <crossdeltavi@gmail.com>
2025-12-07 16:27:48 +0100

    Updates

--- assets/_-ByeKWhhE.js
+++
@@ -1,1 +0,0 @@
-import{p as t,j as e}from"./index-DOAlxn-z.js";import{u as n,B as s}from"./useTypesetOnLoad-CLNHLiin.js";function m(){const{body:o}=t.useLoaderData();return n(),e.jsx(s,{component:"div",dangerouslySetInnerHTML:{__html:o}})}export{m as component};
